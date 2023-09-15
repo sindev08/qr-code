e985350c2e2128aabba4678171b43b6daf33fbde
Singgih Budi Purnadi <57170834+sindev08@users.noreply.github.com>
2023-09-15 15:19:52 +0700

    feat (convert pdf): export pdf (base)

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,129 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:get/get.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  void downloadCatalog() async {
+    final pdf = pw.Document();
+    pdf.addPage(
+      pw.Page(
+          pageFormat: PdfPageFormat.a4,
+          build: (context) {
+            return pw.Column(children: [
+              pw.Center(
+                  child: pw.Text("Catalog Product",
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 24,
+                      ))),
+              pw.SizedBox(height: 20),
+              pw.Table(
+                  border: pw.TableBorder.all(
+                      color: PdfColor.fromHex("#000000"), width: 2),
+                  children: [
+                    pw.TableRow(children: [
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(20),
+                        child: pw.Text("No",
+                            textAlign: pw.TextAlign.center,
+                            style: pw.TextStyle(
+                                fontSize: 12, fontWeight: pw.FontWeight.bold)),
+                      ),
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(20),
+                        child: pw.Text("Product code",
+                            textAlign: pw.TextAlign.center,
+                            style: pw.TextStyle(
+                                fontSize: 12, fontWeight: pw.FontWeight.bold)),
+                      ),
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(20),
+                        child: pw.Text("Nama Barang",
+                            textAlign: pw.TextAlign.center,
+                            style: pw.TextStyle(
+                                fontSize: 12, fontWeight: pw.FontWeight.bold)),
+                      ),
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(20),
+                        child: pw.Text("Quantity",
+                            textAlign: pw.TextAlign.center,
+                            style: pw.TextStyle(
+                                fontSize: 12, fontWeight: pw.FontWeight.bold)),
+                      ),
+                      pw.Padding(
+                        padding: pw.EdgeInsets.all(20),
+                        child: pw.Text("QR Code",
+                            textAlign: pw.TextAlign.center,
+                            style: pw.TextStyle(
+                                fontSize: 12, fontWeight: pw.FontWeight.bold)),
+                      ),
+                    ]),
+                    // table body
+                    ...List.generate(6, (index) {
+                      return pw.TableRow(children: [
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(20),
+                          child: pw.Text("${index + 1}",
+                              textAlign: pw.TextAlign.center,
+                              style: pw.TextStyle(
+                                fontSize: 12,
+                              )),
+                        ),
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(20),
+                          child: pw.Text("${83178 + index}",
+                              textAlign: pw.TextAlign.center,
+                              style: pw.TextStyle(
+                                fontSize: 12,
+                              )),
+                        ),
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(20),
+                          child: pw.Text("Barang ${index + 1}",
+                              textAlign: pw.TextAlign.center,
+                              style: pw.TextStyle(
+                                fontSize: 12,
+                              )),
+                        ),
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(20),
+                          child: pw.Text("3232${1312 + index}",
+                              textAlign: pw.TextAlign.center,
+                              style: pw.TextStyle(
+                                fontSize: 12,
+                              )),
+                        ),
+                        pw.Padding(
+                          padding: pw.EdgeInsets.all(20),
+                          child: pw.Text("QR Code",
+                              textAlign: pw.TextAlign.center,
+                              style: pw.TextStyle(
+                                fontSize: 12,
+                              )),
+                        ),
+                      ]);
+                    })
+                  ])
+            ]);
+          }),
+    );
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+    // simpan
+    Uint8List bytes = await pdf.save();
+
+    // Buat file kosong di direktori
+    final dir = await getApplicationCacheDirectory();
+    final file = File("${dir.path}/mydocument.pdf");
+
+    // memasukkan data bytes -> file kosong
+    await file.writeAsBytes(bytes);
+
+    // buka pdf
+    await OpenFile.open(file.path);
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
