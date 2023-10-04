747eff68709d7b207bd3eabf667b8fea1cebfec7
Singgih Budi Purnadi <57170834+sindev08@users.noreply.github.com>
2023-10-04 20:39:06 +0700

    fix : responsive rotation home page

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -23,6 +23,8 @@ class HomeView extends GetView<HomeController> {
           child: Stack(children: [
             Positioned(
                 top: 32,
+                left: 0,
+                right: 0,
                 child: Container(
                   decoration: BoxDecoration(
                     color: CustomColors.white,
@@ -39,9 +41,9 @@ class HomeView extends GetView<HomeController> {
                     padding: const EdgeInsets.symmetric(
                         vertical: 12, horizontal: 20),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         ProfileSvg(width: 48),
-                        const SizedBox(width: 20),
                         const Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
@@ -60,8 +62,6 @@ class HomeView extends GetView<HomeController> {
                             ),
                           ],
                         ),
-                        const SizedBox(width: 20),
-                        // LogoutSvg(width: 36)
                         IconButton(
                             onPressed: () async {
                               Map<String, dynamic> hasil = await authC.logout();
@@ -76,129 +76,141 @@ class HomeView extends GetView<HomeController> {
                     ),
                   ),
                 )),
-            Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Column(
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.15,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "Selamat datang",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 24),
+                      const Column(
+                        children: [
+                          Text(
+                            "Selamat datang",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700, fontSize: 24),
+                          ),
+                          Text(
+                            "Projek QR Code digunakan untuk pendataan barang secara realtime",
+                            style: TextStyle(fontSize: 12),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
                       ),
-                      Text(
-                        "Projek QR Code digunakan untuk pendataan barang secara realtime",
-                        style: TextStyle(fontSize: 12),
-                        textAlign: TextAlign.center,
+                      const SizedBox(height: 32),
+                      GridView.builder(
+                        itemCount: 4,
+                        shrinkWrap:
+                            true, // Agar GridView dapat digunakan dalam SingleChildScrollView
+                        physics:
+                            const NeverScrollableScrollPhysics(), // Agar GridView tidak melakukan scrolling
+                        padding: const EdgeInsets.all(20),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: MediaQuery.of(context).orientation ==
+                                  Orientation.landscape
+                              ? 4
+                              : 2,
+                          mainAxisSpacing: 20,
+                          crossAxisSpacing: 20,
+                        ),
+                        itemBuilder: (context, index) {
+                          late String title;
+                          late Widget icon;
+                          late VoidCallback onTap;
+
+                          switch (index) {
+                            case 0:
+                              title = "Tambah";
+                              icon = CreateSvg(width: 68);
+                              onTap = () => Get.toNamed(Routes.addProduct);
+                              break;
+                            case 1:
+                              title = "Produk";
+                              icon = ProductSvg(width: 68);
+                              onTap = () => Get.toNamed(Routes.products);
+                              break;
+                            case 2:
+                              title = "Scan";
+                              icon = ScanSvg(width: 68);
+                              onTap = () async {
+                                String barcode =
+                                    await FlutterBarcodeScanner.scanBarcode(
+                                  "#000000",
+                                  "Cancel",
+                                  true,
+                                  ScanMode.QR,
+                                );
+                                // Get data dari firebase, search by product id
+                                Map<String, dynamic> hasil =
+                                    await controller.getProductById(barcode);
+
+                                if (hasil["error"] == false) {
+                                  Get.toNamed(Routes.detailProduct,
+                                      arguments: hasil["data"]);
+                                } else {
+                                  Get.snackbar(
+                                    "Error",
+                                    hasil["message"],
+                                    duration: const Duration(seconds: 2),
+                                  );
+                                }
+                              };
+                              break;
+                            case 3:
+                              title = "Print";
+                              icon = PrintSvg(width: 68);
+                              onTap = () {
+                                controller.downloadCatalog();
+                              };
+                              break;
+                          }
+                          return Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                border: Border.all(
+                                    color: CustomColors.grey.shade200,
+                                    width: 1),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Color.fromRGBO(14, 8, 48, 0.1),
+                                    offset: Offset(0.0, 10.0), //(x,y)
+                                    blurRadius: 20.0,
+                                  ),
+                                ]),
+                            child: Material(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                              child: InkWell(
+                                onTap: onTap,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    icon,
+                                    // Icon(icon, size: 50),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      title,
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ],
                   ),
-                  const SizedBox(height: 32),
-                  GridView.builder(
-                    itemCount: 4,
-                    shrinkWrap:
-                        true, // Agar GridView dapat digunakan dalam SingleChildScrollView
-                    physics:
-                        const NeverScrollableScrollPhysics(), // Agar GridView tidak melakukan scrolling
-                    padding: const EdgeInsets.all(20),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 20,
-                      crossAxisSpacing: 20,
-                    ),
-                    itemBuilder: (context, index) {
-                      late String title;
-                      late Widget icon;
-                      late VoidCallback onTap;
-
-                      switch (index) {
-                        case 0:
-                          title = "Tambah";
-                          icon = CreateSvg(width: 68);
-                          onTap = () => Get.toNamed(Routes.addProduct);
-                          break;
-                        case 1:
-                          title = "Produk";
-                          icon = ProductSvg(width: 68);
-                          onTap = () => Get.toNamed(Routes.products);
-                          break;
-                        case 2:
-                          title = "Scan";
-                          icon = ScanSvg(width: 68);
-                          onTap = () async {
-                            String barcode =
-                                await FlutterBarcodeScanner.scanBarcode(
-                              "#000000",
-                              "Cancel",
-                              true,
-                              ScanMode.QR,
-                            );
-                            // Get data dari firebase, search by product id
-                            Map<String, dynamic> hasil =
-                                await controller.getProductById(barcode);
-
-                            if (hasil["error"] == false) {
-                              Get.toNamed(Routes.detailProduct,
-                                  arguments: hasil["data"]);
-                            } else {
-                              Get.snackbar(
-                                "Error",
-                                hasil["message"],
-                                duration: const Duration(seconds: 2),
-                              );
-                            }
-                          };
-                          break;
-                        case 3:
-                          title = "Print";
-                          icon = PrintSvg(width: 68);
-                          onTap = () {
-                            controller.downloadCatalog();
-                          };
-                          break;
-                      }
-                      return Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            border: Border.all(
-                                color: CustomColors.grey.shade200, width: 1),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Color.fromRGBO(14, 8, 48, 0.1),
-                                offset: Offset(0.0, 10.0), //(x,y)
-                                blurRadius: 20.0,
-                              ),
-                            ]),
-                        child: Material(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
-                          child: InkWell(
-                            onTap: onTap,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                icon,
-                                // Icon(icon, size: 50),
-                                const SizedBox(height: 10),
-                                Text(
-                                  title,
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
+                ),
               ),
             ),
           ]),
